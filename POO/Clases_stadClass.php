79034ac2cd1c8999b26664c417338d4651f8f1f6
Alejo12680 <alejo152725@gmail.com>
2025-05-04 23:25:31 -0500

    Feat: Castear_Arrays_Objetos

--- POO/Clases_stadClass.php
+++ POO/Clases_stadClass.php
@@ -12,3 +12,21 @@ echo gettype($persona); // Imprime el tipo de dato del objeto, en este caso es u
 $persona->nombre = "Pedro"; // Se le asigna un nuevo valor a la propiedad nombre
 $persona->edad = 30; // Se le asigna un nuevo valor a la propiedad edad
 
+// **********************************************************************************************************
+// Vamos castear stbclass en un arrays a objetos dinamicos.
+$array = (array)$persona; // Convierte el objeto en un array
+echo gettype($array); // Imprime el tipo de dato del array, en este caso es un array
+echo $array['nombre']; // Imprime el valor de la propiedad nombre del array, en este caso es "Pedro"
+
+// Creacion de un array y luego convertirlo en un objeto
+$arrayLocation = [
+  "address" => "Calle mal 123",
+  "city" => "Madrid"
+];
+
+// Se castea el array a un objeto, se convierte el array en un objeto
+$objLocation = (object)$arrayLocation; // Convierte el array en un objeto
+echo $objLocation->address; // Imprime el valor de la propiedad address del objeto, en este caso es "Calle mal 123"
+echo $objLocation->city; // Imprime el valor de la propiedad city del objeto, en este caso es "Madrid"
+print_r($objLocation); // Imprime un stdClass Object con las propiedades address y city
+
